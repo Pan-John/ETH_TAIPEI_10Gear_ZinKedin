db1b0ff9b2d58f568bc9929b9a3b3c7b2265da1c
Pan <125814787+Pan-John@users.noreply.github.com>
2023-04-22 11:58:12 +0800

    Update com.sol
    change var name, add apply function for applier

--- com.sol
+++ com.sol
@@ -1,47 +1,56 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract Company{
-    address public owner; 
-    uint256 public work_num;
-    //mapping(uint256 => string) formWithExp;
-    string description;
-    
 
-    function initialize(uint256 _work_num, string memory _description)public{
+// this contract is for company to issue a hiring info. and let labors to apply
+contract Company{
+    address public owner;// should be company's address that issues this info
+    string public job_title; 
+    string public description;
+    //mapping(uint256 => string) formWithExp;
+    mapping (address applier => address cv_address ) public applications; // stores applier's applications
+
+
+    // For companies to issue hiring info.
+    function initialize(string memory _job_title, string memory _description)public{
         owner = msg.sender;
         description = _description;
-        work_num = _work_num;
+        job_title = _job_title;
         //condition_num = _condition_num;
         //createForm(condition_num);
     }
+
+    // For company to return appliers' applications
+    function getApplications(address _cv_address) public returns (address[] memory, address[] memory){
+
+    }
+    
+
 
     /*function createForm(uint256 _condition_num) public {
         for (uint i = 0; i < _condition_num; i++) {
             formWithExp[i] = ;
         }
     }*/
-    /*
-    function getdescription() public returns(string memory _description){
-        description = _description;
-        return description;
-    }*/
-    
-    function getWorkNum() public view returns(uint256){
-        return work_num;
+
+
+    // UI functions:
+    // to see the job title and descriptions
+    function getJobTitle() public view returns(string memory _job_title){
+        return job_title;
     }
 
-    function getDescription()public view returns(string memory){
-        return  description;
+    function getdescription() public view returns(string memory _description){
+        return description;
+    }
+
+    function ApplyForJob(address _cv_address) public{
+        applications[msg.sender] = _cv_address;
     }
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only the owner can call this function.");
         _;
     } 
-
-    function applythework(){
-        
-    }
     
 }
